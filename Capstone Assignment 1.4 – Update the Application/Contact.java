0936c1c522b5c52fb22930b9060fc85f3606f4bc
qs_ashish05 <aashishjha8080@gmail.com>
2026-03-09 22:11:01 +0530

    Capstone Assignment 1.4 – Update the Application

---
+++ Capstone Assignment 1.4 – Update the Application/Contact.java
@@ -0,0 +1,56 @@
+package com.example.sharingapp;
+
+/**
+ * Contact class
+ */
+public class Contact {
+
+    private String username;
+    private String email;
+    private String id;
+
+    public Contact(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.id = java.util.UUID.randomUUID().toString();
+    }
+
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+        if (id == null) {
+            this.id = java.util.UUID.randomUUID().toString();
+        } else {
+            this.id = id;
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
---
+++ Capstone Assignment 1.4 – Update the Application/contact_submission/submission/ContactList.java
@@ -0,0 +1,101 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+/**
+ * ContactList class
+ */
+public class ContactList {
+
+    private ArrayList<Contact> contacts;
+    private String FILENAME = "contacts.sav";
+
+    public ContactList() {
+        contacts = new ArrayList<Contact>();
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        contacts.remove(contact);
+    }
+
+    public Contact getContact(int index) {
+        return contacts.get(index);
+    }
+
+    public int getIndex(Contact contact) {
+        int pos = 0;
+        for (Contact c : contacts) {
+            if (contact.getId().equals(c.getId())) {
+                return pos;
+            }
+            pos++;
+        }
+        return -1;
+    }
+
+    public int getSize() {
+        return contacts.size();
+    }
+
+    public ArrayList<String> getUsernameList() {
+        ArrayList<String> usernames = new ArrayList<>();
+        for (Contact c : contacts) {
+            usernames.add(c.getUsername());
+        }
+        return usernames;
+    }
+
+    public void loadContacts(Context context) {
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<Contact>>() {}.getType();
+            contacts = gson.fromJson(isr, listType);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            contacts = new ArrayList<Contact>();
+        } catch (IOException e) {
+            contacts = new ArrayList<Contact>();
+        }
+    }
+
+    public void saveContacts(Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(contacts, osw);
+            osw.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
